5553c0af7446f69aeab33a6bb0a97249731d9440
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-10-31 15:25:20 +0300

    edit Stars and Star

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -1,18 +1,20 @@
-function getStars() {
-    return (
-        <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
-            <path d="M0 0h18v18H0z" fill="none"/>
-        </svg>
-    )
-}
+import { Props } from "./Stars";
 
-function Star(count: number) {
-    let content = getStars();
-    for(let i = 0; i < count; i++) {
-        getStars()
+function Star({count} : Props){
+    
+    if(count >=1 && count <= 5) {
+        const arrSrars = [];
+        for(let i = 0; i < count; i++) {
+            arrSrars.push(i);
+        }
+        const stars = arrSrars.map(index =>
+            <svg key = {index} fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
+                <path d="M0 0h18v18H0z" fill="none"/>
+            </svg>
+        )
+        return stars
     }
-    return content;
 }
 
 export default Star
--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -4,20 +4,17 @@ export interface Props {
     count: number
   }
 
-//Stars.defaultProps = {count:0};
-
 function Stars({count} : Props){
 
-    if(count >=1 && count <= 5)
-        return (
-            <>
-                <ul className="card-body-stars u-clearfix">
-                    <li>
-                        <Star count={count}/>    
-                    </li>
-                </ul>
-            </>
-        )
+    return (
+        <>
+            <ul className="card-body-stars u-clearfix">
+                <li>
+                    <Star count={count}/>    
+                </li>
+            </ul>
+        </>
+    )
 }
 
 export default Stars
